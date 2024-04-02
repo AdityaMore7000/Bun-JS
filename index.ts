05c7751b63abfae92be48439dfdc04554c09b48a
adityamore7000 <adi.more7000@gmail.com>
2024-04-02 10:16:33 +0530

    create server

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 const server = Bun.serve({
     port: 3000,
     fetch(req){
-        return new Response('Hello World!')
+        const url = new URL(req.url);
+        if(url.pathname === '/'){
+            return Response.json({msg:'Hello World!'})
+        }
+        else if(url.pathname === '/about'){
+            return Response.json({msg:'About Us'})
+        }
+        return new Response(JSON.stringify({status:404,msg:'Not Found'}));
     }
 })
 
